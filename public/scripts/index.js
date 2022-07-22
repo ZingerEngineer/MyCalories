e70aec38a215d9d39e1fee7b0817a8c54879aa83
ZingerEngineer <ramyisez@gmail.com>
2022-07-22 02:05:48 +0200

    edit product

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,31 @@
+async ()=>{
+    try {
+  
+      await axios.get("/api/product/")
+      console.log(products)
+  
+    } catch (error) {
+      
+    }  
+  }
 const createButton = document.querySelector(".btn-1")
+const editButton = document.querySelector(".edit-button")
+const deleteButton = document.querySelector(".delete-button")
+
 createButton.addEventListener("click",()=>{
     window.location.replace("/pages/new-product.html")
+})
+
+editButton.addEventListener("click",()=>{
+    window.location.replace("/pages/edit-product.html")
+})
+
+deleteButton.addEventListener("click",async ()=>{
+  try {
+
+    await axios.delete(`/api/product/${id}`)
+
+  } catch (error) {
+    
+  }  
 })
