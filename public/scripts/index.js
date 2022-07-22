08b2bd2a91849cee19217ba52bf2e4403a216fc6
ZingerEngineer <ramyisez@gmail.com>
2022-07-23 01:24:53 +0200

    cards done with no delete function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,31 +1,83 @@
-async ()=>{
-    try {
-  
-      await axios.get("/api/product/")
-      console.log(products)
-  
-    } catch (error) {
-      
-    }  
-  }
-const createButton = document.querySelector(".btn-1")
-const editButton = document.querySelector(".edit-button")
-const deleteButton = document.querySelector(".delete-button")
+const productList = document.querySelector(".product-list");
+const createButton = document.querySelector(".btn-1");
+window.addEventListener("load", async () => {
+  const res = await axios.get("/api/product");
+  let productArray = res.data;
+  productArray.forEach((obj) => {
+    let objId = obj._id;
+    let objName = obj.name;
+    let objCals = obj.kCaloryPerGm;
 
-createButton.addEventListener("click",()=>{
-    window.location.replace("/pages/new-product.html")
-})
+    const editIcon = document.createElement("I");
+    editIcon.classList.add("fa-solid", "fa-square-pen");
+    const deleteIcon = document.createElement("I");
+    deleteIcon.classList.add("fa-solid", "fa-square-xmark");
+    const deleteButton = document.createElement("BUTTON");
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", async () => {
+      try {
+        swal({
+          title: "Are you sure ?",
+          text: `Do you want to delete ${objName}`,
+          icon: "warning",
+          buttons: ["Cancel", "Yes"],
+          dangerMode: true,
+        }).then((willDelete) => {
+          if (willDelete) {
+            swal(`${objName} is deleted.`, {
+              icon: "success",
+            });
+            console.log("here")
+            let id = product.id;
+            product.remove();
+            console.log(id)
+            async () => {
+             const res = await axios.delete("/api/product/",{
+                params: {
+                  ID: id
+                }
+              })
+              console.log(res)
+         console.log("here")};
+          } else {
+            swal("Delete canceled.");
+          }
+        });
+      } catch (error) {}
+    });
+    deleteButton.appendChild(deleteIcon);
+    const editButton = document.createElement("BUTTON");
+    editButton.classList.add("edit-button");
+    editButton.addEventListener("click", () => {
+      window.location.assign("/pages/edit-product.html");
+    });
+    editButton.appendChild(editIcon);
+    const productButtons = document.createElement("DIV");
+    productButtons.classList.add("product-buttons");
+    productButtons.appendChild(deleteButton);
+    productButtons.appendChild(editButton);
+    const productName = document.createElement("P");
+    productName.classList.add("product-name");
+    productName.innerText = objName;
+    const productCals = document.createElement("P");
+    productCals.classList.add("product-Kcalgm");
+    productCals.innerText = objCals;
+    const productInfo = document.createElement("DIV");
+    productInfo.classList.add("product-info");
+    productInfo.appendChild(productName);
+    productInfo.appendChild(productCals);
+    const product = document.createElement("DIV");
+    product.appendChild(productInfo);
+    product.appendChild(productButtons);
+    product.classList.add("product");
+    product.id = objId;
+    productList.appendChild(product);
+  });
+});
 
-editButton.addEventListener("click",()=>{
-    window.location.replace("/pages/edit-product.html")
-})
+createButton.addEventListener("click", () => {
+  window.location.assign("/pages/new-product.html");
+});
+const editButton = document.querySelector(".edit-button");
 
-deleteButton.addEventListener("click",async ()=>{
-  try {
-
-    await axios.delete(`/api/product/${id}`)
-
-  } catch (error) {
-    
-  }  
-})
+const deleteButton = document.querySelector(".delete-button");
