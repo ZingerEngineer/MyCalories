e2acc1819c713e7420295da94f2b8b58cac1fbc8
ZingerEngineer <ramyisez@gmail.com>
2022-08-02 19:04:46 +0200

    creating & editing products done

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -19,7 +19,7 @@ const wordCheck = (inputNameEditElement) => {
   } else {
     isProduct = false;
   }
-  console.log(isProduct)
+  console.log(isProduct);
 };
 const positiveCheck = (inputCalsEditElement) => {
   if (positiveNumberRegEx.test(inputCalsEditElement.value)) {
@@ -27,10 +27,14 @@ const positiveCheck = (inputCalsEditElement) => {
   } else {
     isPositive = false;
   }
-  console.log(isPositive)
+  console.log(isPositive);
 };
-
-const nameInputEditCreate = (nameInputEditId,value) => {
+const dimmerCreate = () => {
+  const dimmer = document.createElement("DIV");
+  dimmer.classList.add("dimmer");
+  return dimmer;
+};
+const nameInputEditCreate = (nameInputEditId, value) => {
   const inputNameEdit = document.createElement("INPUT");
   inputNameEdit.classList.add("input-name-edit");
   inputNameEdit.setAttribute("type", "text");
@@ -38,11 +42,10 @@ const nameInputEditCreate = (nameInputEditId,value) => {
   inputNameEdit.setAttribute("id", `${nameInputEditId}-name-input-edit`);
   inputNameEdit.setAttribute("value", value);
 
-
   return inputNameEdit;
 };
 
-const calsInputEditCreate = (calsInputEditId,value) => {
+const calsInputEditCreate = (calsInputEditId, value) => {
   const inputCalsEdit = document.createElement("INPUT");
   inputCalsEdit.classList.add("input-cals-edit");
   inputCalsEdit.setAttribute("type", "number");
@@ -50,18 +53,28 @@ const calsInputEditCreate = (calsInputEditId,value) => {
   inputCalsEdit.setAttribute("id", `${calsInputEditId}-cals-input-edit`);
   inputCalsEdit.setAttribute("value", value);
 
-
   return inputCalsEdit;
 };
 
 const confirmButtonCreate = (confirmButtonId) => {
   const confirmIcon = document.createElement("I");
-  confirmIcon.classList.add("fa-solid", "fa-square-check");
+  confirmIcon.classList.add("fa-solid", "fa-check");
   const confirmButton = document.createElement("BUTTON");
   confirmButton.setAttribute("id", `${confirmButtonId}-confirm-button`);
   confirmButton.classList.add("confirm-button");
+  confirmButton.classList.add("card-button");
   confirmButton.appendChild(confirmIcon);
   return confirmButton;
+};
+const cancelEditButtonCreate = (cancelButtonId) => {
+  const cancelIcon = document.createElement("I");
+  cancelIcon.classList.add("fa-solid", "fa-xmark");
+  const cancelButton = document.createElement("BUTTON");
+  cancelButton.setAttribute("id", `${cancelButtonId}-cancel-button`);
+  cancelButton.classList.add("cancel-button");
+  cancelButton.classList.add("card-button");
+  cancelButton.appendChild(cancelIcon);
+  return cancelButton;
 };
 
 const deleteProduct = async (objId) => {
@@ -69,52 +82,57 @@ const deleteProduct = async (objId) => {
   let productCard = document.getElementById(objId);
   productCard.remove();
 };
+
+const deletion = async (objName, objId) => {
+  try {
+    swal({
+      title: "Are you sure ?",
+      text: `Do you want to delete ${objName}`,
+      icon: "warning",
+      buttons: ["Cancel", "Yes"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal(`${objName} is deleted.`, {
+          icon: "success",
+        });
+        deleteProduct(objId);
+      } else {
+        swal("Delete canceled.");
+      }
+    });
+  } catch (error) {
+    Toastify({
+      text: "Error happened",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, red, red)",
+      },
+    }).showToast();
+  }
+};
+
 const deleteButtonCreate = (objName, objId, deleteButtonId) => {
   const deleteIcon = document.createElement("I");
-  deleteIcon.classList.add("fa-solid", "fa-square-xmark");
+  deleteIcon.classList.add("fa-solid", "fa-trash");
   const deleteButton = document.createElement("BUTTON");
   deleteButton.classList.add("delete-button");
+  deleteButton.classList.add("card-button");
   deleteButton.setAttribute("id", `${deleteButtonId}-delete-button`);
   deleteButton.appendChild(deleteIcon);
-  deleteButton.addEventListener("click", async () => {
-    try {
-      swal({
-        title: "Are you sure ?",
-        text: `Do you want to delete ${objName}`,
-        icon: "warning",
-        buttons: ["Cancel", "Yes"],
-        dangerMode: true,
-      }).then((willDelete) => {
-        if (willDelete) {
-          swal(`${objName} is deleted.`, {
-            icon: "success",
-          });
-          deleteProduct(objId);
-        } else {
-          swal("Delete canceled.");
-        }
-      });
-    } catch (error) {
-      Toastify({
-        text: "Error happened",
-        duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "linear-gradient(to right, red, red)",
-        },
-      }).showToast();
-    }
-  });
+  deleteButton.addEventListener("click", () => deletion(objName, objId));
   return deleteButton;
 };
 const editButtonCreate = (editButtonId) => {
   const editIcon = document.createElement("I");
-  editIcon.classList.add("fa-solid", "fa-square-pen");
+  editIcon.classList.add("fa-solid", "fa-pen");
   const editButton = document.createElement("BUTTON");
   editButton.classList.add("edit-button");
+  editButton.classList.add("card-button");
   editButton.setAttribute("id", `${editButtonId}-edit-button`);
   editButton.appendChild(editIcon);
   return editButton;
@@ -194,11 +212,25 @@ window.addEventListener("load", async function renderer() {
     );
 
     const editFunction = (productElement) => {
-      let productName = document.getElementById(`${productNameId}-product-name`)
-      let productCals = document.getElementById(`${productCalsId}-product-cals`)
-      let inputNameEditElement = nameInputEditCreate(productElement.id,productName.innerText);
+      productElement.style.zIndex = 2;
+      let dimmerElement = dimmerCreate();
+      let container = document.getElementsByClassName("container").item(0);
+      container.insertBefore(dimmerElement, productList);
+      let productName = document.getElementById(
+        `${productNameId}-product-name`
+      );
+      let productCals = document.getElementById(
+        `${productCalsId}-product-cals`
+      );
+      let inputNameEditElement = nameInputEditCreate(
+        productElement.id,
+        productName.innerText
+      );
 
-      let inputCalsEditElement = calsInputEditCreate(productElement.id,productCals.innerText);
+      let inputCalsEditElement = calsInputEditCreate(
+        productElement.id,
+        productCals.innerText
+      );
       inputCalsEditElement.addEventListener("keydown", function (event) {
         if (invalidChars.includes(event.key)) {
           event.preventDefault();
@@ -208,20 +240,80 @@ window.addEventListener("load", async function renderer() {
         `${productElement.id}-edit-button`
       );
       toBeDeletedEditButton.remove();
-      let tobeDeletedProductName = document.getElementById(`${productElement.id}-product-name`)
-      let tobeDeletedProductCals = document.getElementById(`${productElement.id}-product-cals`)
+      let tobeDeletedProductName = document.getElementById(
+        `${productElement.id}-product-name`
+      );
+      let tobeDeletedProductCals = document.getElementById(
+        `${productElement.id}-product-cals`
+      );
       tobeDeletedProductName.remove();
       tobeDeletedProductCals.remove();
 
       productInfoElement.appendChild(inputNameEditElement);
       productInfoElement.appendChild(inputCalsEditElement);
-      let toBeAddedInputNameEdit = document.getElementById(`${productElement.id}-name-input-edit`)
-      toBeAddedInputNameEdit.addEventListener("input",()=>wordCheck(inputNameEditElement))
+      let toBeAddedInputNameEdit = document.getElementById(
+        `${productElement.id}-name-input-edit`
+      );
+      toBeAddedInputNameEdit.addEventListener("input", () =>
+        wordCheck(inputNameEditElement)
+      );
 
-      let toBeAddedInputCalsEdit = document.getElementById(`${productElement.id}-cals-input-edit`)
-      toBeAddedInputCalsEdit.addEventListener("input",()=> positiveCheck(inputCalsEditElement))
+      let toBeAddedInputCalsEdit = document.getElementById(
+        `${productElement.id}-cals-input-edit`
+      );
+      toBeAddedInputCalsEdit.addEventListener("input", () =>
+        positiveCheck(inputCalsEditElement)
+      );
+      let toBeDeletedDeleteButton = document.getElementById(
+        `${productElement.id}-delete-button`
+      );
+      toBeDeletedDeleteButton.remove();
 
+      const cancelButtonElement = cancelEditButtonCreate(productElement.id);
+      cancelButtonElement.addEventListener("click", () => {
+        let dimmer = document.getElementsByClassName("dimmer").item(0);
+        dimmer.remove();
+        let toBeDeletedCancelButton = document.getElementById(
+          `${productElement.id}-cancel-button`
+        );
+        toBeDeletedCancelButton.remove();
 
+        let toBeDeletedConfirmButton = document.getElementById(
+          `${productElement.id}-confirm-button`
+        );
+        toBeDeletedConfirmButton.remove();
+        const editButtonElement = editButtonCreate(productElement.id);
+        editButtonElement.addEventListener("click", () =>
+          editFunction(productElement)
+        );
+        const deleteButtonElement = deleteButtonCreate(
+          productName.innerText,
+          productElement.id,
+          productElement.id
+        );
+        deleteButtonElement.addEventListener("click", () => deletion);
+
+        productButtonsElement.appendChild(editButtonElement);
+        productButtonsElement.appendChild(deleteButtonElement);
+        const toBeDeletedInputNameEdit = document.getElementById(
+          `${productElement.id}-name-input-edit`
+        );
+        const toBeDeletedInputCalsEdit = document.getElementById(
+          `${productElement.id}-cals-input-edit`
+        );
+        toBeDeletedInputNameEdit.remove();
+        toBeDeletedInputCalsEdit.remove();
+        let productNameElement = productNameCreate(
+          productName.innerText,
+          productElement.id
+        );
+        let productCalsElement = productCalsCreate(
+          productCals.innerText,
+          productElement.id
+        );
+        productInfoElement.appendChild(productNameElement);
+        productInfoElement.appendChild(productCalsElement);
+      });
       const confirmButtonElement = confirmButtonCreate(productElement.id);
       confirmButtonElement.addEventListener("click", async () => {
         try {
@@ -244,10 +336,16 @@ window.addEventListener("load", async function renderer() {
               background: "linear-gradient(315deg, #7ee8fa 0%, #80ff72 74%)",
             },
           }).showToast();
+          productElement.style.zIndex = 0;
+          let toBeDeletedCancelButton = document.getElementById(
+            `${productElement.id}-cancel-button`
+          );
+          toBeDeletedCancelButton.remove();
           let name = editedProduct.name;
           let cals = editedProduct.kCaloryPerGm;
           let productNameElement = productNameCreate(name, productElement.id);
           let productCalsElement = productCalsCreate(cals, productElement.id);
+          let dimmer = document.getElementsByClassName("dimmer").item(0);
           const toBeDeletedInputNameEdit = document.getElementById(
             `${productElement.id}-name-input-edit`
           );
@@ -256,6 +354,7 @@ window.addEventListener("load", async function renderer() {
           );
           toBeDeletedInputNameEdit.remove();
           toBeDeletedInputCalsEdit.remove();
+          dimmer.remove();
           productInfoElement.appendChild(productNameElement);
           productInfoElement.appendChild(productCalsElement);
           const toBeDeletedConfirmButton = document.getElementById(
@@ -263,12 +362,14 @@ window.addEventListener("load", async function renderer() {
           );
           toBeDeletedConfirmButton.remove();
           const editButtonElement = editButtonCreate(productElement.id);
-          editButtonElement.addEventListener("click", () => editFunction(productElement));
-          const productButtonsElement = document.getElementById(`${productElement.id}-product-buttons`)
-          productButtonsElement.insertBefore(
-            editButtonElement,
-            deleteButtonElement
+          editButtonElement.addEventListener("click", () =>
+            editFunction(productElement)
           );
+          const productButtonsElement = document.getElementById(
+            `${productElement.id}-product-buttons`
+          );
+          productButtonsElement.appendChild(editButtonElement);
+          productButtonsElement.appendChild(deleteButtonElement);
         } catch (error) {
           console.log(error);
           Toastify({
@@ -285,7 +386,8 @@ window.addEventListener("load", async function renderer() {
         }
       });
       productButtonsElement.appendChild(confirmButtonElement);
-    }
+      productButtonsElement.appendChild(cancelButtonElement);
+    };
 
     let productElement = productCreate(
       objId,
@@ -293,11 +395,9 @@ window.addEventListener("load", async function renderer() {
       productButtonsElement
     );
     productList.appendChild(productElement);
-    editButtonElement.addEventListener(
-      "click",
-      () => editFunction(productElement)
+    editButtonElement.addEventListener("click", () =>
+      editFunction(productElement)
     );
-
   });
 });
 
